0f71022d312ce26516410afb9ab8b4f4568d1046
Ivan <madritistaaa@gmail.com>
2017-11-19 21:06:14 +0200

    Add db context interface

---
+++ src/SecretSanta/SecretSanta.Data/Contracts/ISecretSantaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SecretSanta.Data.Contracts
+{
+    public interface ISecretSantaContext
+    {
+        DbSet<TEntity> DbSet<TEntity>()
+            where TEntity : class;
+
+        int SaveChanges();
+
+        void SetAdded<TEntry>(TEntry entity)
+            where TEntry : class;
+
+        void SetDeleted<TEntry>(TEntry entity)
+            where TEntry : class;
+
+        void SetUpdated<TEntry>(TEntry entity)
+where TEntry : class;
+    }
+}
