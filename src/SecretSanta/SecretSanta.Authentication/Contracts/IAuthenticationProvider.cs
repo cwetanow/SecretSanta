0615b1b30f85e4b6ded72fc8e7b3bfdd2d432de4
Ivan <madritistaaa@gmail.com>
2017-11-22 22:06:17 +0200

    Interface for auth provider

---
+++ src/SecretSanta/SecretSanta.Authentication/Contracts/IAuthenticationProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SecretSanta.Models;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Authentication.Contracts
+{
+    public interface IAuthenticationProvider
+    {
+        Task<User> FindByEmailAsync(string email);
+
+        Task<SignInResult> CheckPasswordSignInAsync(User user, string password);
+
+        Task<IdentityResult> RegisterUser(User user, string password);
+
+        Task SignOutAsync();
+    }
+}
