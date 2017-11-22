ea718638cfa01fbd835a18c573e1c1792684d242
Ivan <madritistaaa@gmail.com>
2017-11-22 22:06:27 +0200

    Scaffold login view model

---
+++ src/SecretSanta/SecretSanta.Web/Models/Account/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SecretSanta.Web.Models.Account
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
