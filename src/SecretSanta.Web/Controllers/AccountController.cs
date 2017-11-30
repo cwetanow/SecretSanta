2613a0b72f829f2ebf2adf93d46bea8cb34ea97b
Ivan <madritistaaa@gmail.com>
2017-11-30 13:26:26 +0200

    Add logic for register

--- src/SecretSanta.Web/Controllers/AccountController.cs
+++ src/SecretSanta.Web/Controllers/AccountController.cs
@@ -1,41 +1,62 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SecretSanta.Authentication.Contracts;
 using SecretSanta.Web.Models.Account;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SecretSanta.Factories;
 
 namespace SecretSanta.Web.Controllers
 {
-    [Authorize]
-    [Route("api/[controller]/[action]")]
+    [Route("api/account")]
     public class AccountController : Controller
     {
         private readonly IAuthenticationProvider authenticationProvider;
-        private readonly ITokenProvider tokenProvider;
+        private readonly IUserFactory userFactory;
 
-        public AccountController(IAuthenticationProvider authenticationProvider, 
-            ITokenProvider tokenProvider)
+        public AccountController(IAuthenticationProvider authenticationProvider,
+            IUserFactory userFactory)
         {
             this.authenticationProvider = authenticationProvider;
-            this.tokenProvider = tokenProvider;
+            this.userFactory = userFactory;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login([FromBody]LoginViewModel model)
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
         {
-            var user = await this.authenticationProvider.FindByEmailAsync(model.Email);
+            var user = await this.authenticationProvider.FindByUsernameAsync(model.Username);
 
-            if (user != null)
+            if (user == null)
             {
-                var result = await this.authenticationProvider.CheckPasswordSignInAsync(user, model.Password);
+                user = this.userFactory.CreateUser(model.Username, model.Email, model.DisplayName);
 
-                if (result.Succeeded)
+                var result = await this.authenticationProvider.RegisterUser(user, model.Password);
+
+                if (result == IdentityResult.Success)
                 {
-                    // Generate token
+                    return this.Ok(user);
                 }
             }
 
             return this.BadRequest();
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Login([FromBody]LoginViewModel model)
+        //{
+        //    var user = await this.authenticationProvider.FindByEmailAsync(model.Email);
+
+        //    if (user != null)
+        //    {
+        //        var result = await this.authenticationProvider.CheckPasswordSignInAsync(user, model.Password);
+
+        //        if (result.Succeeded)
+        //        {
+        //            // Generate token
+        //        }
+        //    }
+
+        //    return this.BadRequest();
+        //}
     }
 }
