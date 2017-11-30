e6637804d8b637de7314a8c8735e0d4ad57176b2
Ivan <madritistaaa@gmail.com>
2017-11-30 14:14:06 +0200

    Add account controller constructor tests

---
+++ src/SecretSanta.Web.Tests/Controllers/AccountControllerTests/ConstructorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using SecretSanta.Authentication.Contracts;
+using SecretSanta.Factories;
+using SecretSanta.Web.Controllers;
+
+namespace SecretSanta.Web.Tests.Controllers.AccountControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedFactory = new Mock<IUserFactory>();
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            // Act
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void TestConstructor_PassAuthenticationProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedFactory = new Mock<IUserFactory>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new AccountController(null, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassUserFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new AccountController(mockedProvider.Object, null));
+        }
+    }
+}
