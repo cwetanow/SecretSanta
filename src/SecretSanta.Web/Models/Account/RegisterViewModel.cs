73af08b87ca79c33ba0dfbe37d7cc86e48df979d
Ivan <madritistaaa@gmail.com>
2017-11-30 12:01:09 +0200

    Add register vm

---
+++ src/SecretSanta.Web/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SecretSanta.Web.Models.Account
+{
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
