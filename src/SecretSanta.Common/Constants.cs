a10d9d11b91752ff00f0114e07869174d665daa8
Ivan <madritistaaa@gmail.com>
2017-12-04 20:57:24 +0200

    Add project with constants

---
+++ src/SecretSanta.Common/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace SecretSanta.Common
+{
+    public static class Constants
+    {
+        // Account
+        public const string UserAlreadyExists = "User already exists";
+        public const string InvalidCredentials = "Invalid Credentials";
+    }
+}
