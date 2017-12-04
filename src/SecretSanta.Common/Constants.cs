9f4af18b44fbbab90e2cf3228c3b879c4f8b8988
Ivan <madritistaaa@gmail.com>
2017-12-04 22:08:54 +0200

    Add more constants

--- src/SecretSanta.Common/Constants.cs
+++ src/SecretSanta.Common/Constants.cs
@@ -2,8 +2,14 @@
 {
     public static class Constants
     {
-        // Account
         public const string UserAlreadyExists = "User already exists";
         public const string InvalidCredentials = "Invalid Credentials";
+
+        public const string TokenKey = "Tokens:Key";
+        public const string TokenIssuer = "Tokens:Issuer";
+
+        public const int TokenExpireHours = 2;
+        public const int TokenExpireMinutes = 0;
+        public const int TokenExpireSeconds = 0;
     }
 }
