2b5051ae8adbb34e459c43daa3c6325e5b1aa1f1
Ivan <madritistaaa@gmail.com>
2017-12-04 21:16:34 +0200

    Create token dto

---
+++ src/SecretSanta.Web/Models/Account/TokenDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SecretSanta.Web.Models.Account
+{
+    public class TokenDto
+    {
+        public TokenDto()
+        {
+
+        }
+
+        public TokenDto(string token) : this()
+        {
+            this.Token = token;
+        }
+
+        public string Token { get; set; }
+    }
+}
