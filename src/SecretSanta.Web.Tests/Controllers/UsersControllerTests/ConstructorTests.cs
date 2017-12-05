ed7ef3884dba59ed26adefca0309605be8b1c68a
Ivan <madritistaaa@gmail.com>
2017-12-05 10:40:33 +0200

    Add tests for controller constructor

---
+++ src/SecretSanta.Web.Tests/Controllers/UsersControllerTests/ConstructorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using SecretSanta.Authentication.Contracts;
+using SecretSanta.Factories;
+using SecretSanta.Services.Contracts;
+using SecretSanta.Web.Controllers;
+using SecretSanta.Web.Infrastructure;
+
+namespace SecretSanta.Web.Tests.Controllers.UsersControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedDtoFactory = new Mock<IDtoFactory>();
+            var mockedService = new Mock<IUserService>();
+
+            // Act
+            var controller = new UsersController(mockedService.Object, mockedDtoFactory.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void TestConstructor_PassUserServiceNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedDtoFactory = new Mock<IDtoFactory>();
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new UsersController(null, mockedDtoFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassDtoFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedService = new Mock<IUserService>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new UsersController(mockedService.Object, null));
+        }
+    }
+}
