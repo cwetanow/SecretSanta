7f0fbadf35f0d1063bad446675623b451bd3b0ff
Ivan <madritistaaa@gmail.com>
2017-12-07 15:10:41 +0200

    Add tests for repository methods

---
+++ src/SecretSanta.Data.Tests/RepositoryTests/UpdateTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using SecretSanta.Data.Contracts;
+using SecretSanta.Data.Tests.RepositoryTests.Fakes;
+
+namespace SecretSanta.Data.Tests.RepositoryTests
+{
+    [TestFixture]
+    public class UpdateTests
+    {
+        [Test]
+        public void TestUpdate_ShouldCallDbContextSetUpdated()
+        {
+            // Arrange
+            var mockedDbContext = new Mock<IDbContext>();
+
+            var repository = new EfRepository<FakeRepositoryType>(mockedDbContext.Object);
+
+            var entity = new Mock<FakeRepositoryType>();
+
+            // Act
+            repository.Update(entity.Object);
+
+            // Assert
+            mockedDbContext.Verify(c => c.SetUpdated(entity.Object), Times.Once);
+        }
+    }
+}
