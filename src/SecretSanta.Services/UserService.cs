a6f2f50a29caa5988841ccf476f58bbf58a38689
Ivan <madritistaaa@gmail.com>
2017-12-07 15:29:18 +0200

    Implement GetUsers method in user service

---
+++ src/SecretSanta.Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using SecretSanta.Models;
+using SecretSanta.Services.Contracts;
+using SecretSanta.Data.Contracts;
+using System;
+using System.Linq;
+
+namespace SecretSanta.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> repository;
+
+        public UserService(IRepository<User> repository)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            this.repository = repository;
+        }
+
+        public IEnumerable<User> GetUsers(int offset, int limit, bool sortAscending = true, string searchPattern = null)
+        {
+            var result = this.repository.All;
+
+            if (!string.IsNullOrEmpty(searchPattern))
+            {
+                result = result
+                    .Where(u => u.DisplayName.Contains(searchPattern) || u.UserName.Contains(searchPattern));
+            }
+
+            if (sortAscending)
+            {
+                result = result
+                    .OrderBy(u => u.DisplayName);
+            }
+            else
+            {
+                result = result
+                    .OrderByDescending(u => u.DisplayName);
+            }
+
+            result = result
+                .Skip(offset)
+                .Take(limit);
+
+            return result.ToList();
+        }
+    }
+}
