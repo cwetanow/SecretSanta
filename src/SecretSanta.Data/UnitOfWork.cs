4d377c00702fe97d23d0952536996362e0eaa1cb
Ivan <madritistaaa@gmail.com>
2017-12-07 14:33:17 +0200

    Implement unit of work

---
+++ src/SecretSanta.Data/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using SecretSanta.Data.Contracts;
+using System;
+
+namespace SecretSanta.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbContext dbContext;
+
+        public UnitOfWork(IDbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            this.dbContext = dbContext;
+        }
+
+        public Task<int> CommitAsync()
+        {
+            return this.dbContext.SaveChangesAsync();
+        }
+    }
+}
