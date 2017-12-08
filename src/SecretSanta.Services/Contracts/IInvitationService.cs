940169000e6e9384de32880c6288caf947631fbb
Ivan <madritistaaa@gmail.com>
2017-12-08 14:06:41 +0200

    Create invite service

---
+++ src/SecretSanta.Services/Contracts/IInvitationService.cs
@@ -0,0 +1,10 @@
+﻿using SecretSanta.Models;
+using System.Collections.Generic;
+
+namespace SecretSanta.Services.Contracts
+{
+    public interface IInviteService
+    {
+        IEnumerable<Invite> GetPendingInvites(string userId);
+    }
+}
---
+++ src/SecretSanta.Services/InviteService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using SecretSanta.Models;
+using SecretSanta.Services.Contracts;
+using SecretSanta.Data.Contracts;
+using SecretSanta.Factories;
+using System.Linq;
+using SecretSanta.Models.Enums;
+
+namespace SecretSanta.Services
+{
+    public class InviteService : IInviteService
+    {
+        private readonly IRepository<Invite> repository;
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IInviteFactory factory;
+
+        public InviteService(IRepository<Invite> repository, IUnitOfWork unitOfWork, IInviteFactory factory)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+            this.factory = factory;
+        }
+
+        public IEnumerable<Invite> GetPendingInvites(string userId)
+        {
+            var invites = this.repository.All
+                .Where(i => i.State == InviteState.Pending && i.UserId.Equals(userId))
+                .ToList();
+
+            return invites;
+        }
+    }
+}
