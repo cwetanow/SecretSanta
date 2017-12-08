5e711f090d9bcd21780eb76382e62b88a3214707
Ivan <madritistaaa@gmail.com>
2017-12-08 10:29:59 +0200

    Add constructor and group name to model

--- src/SecretSanta.Models/Group.cs
+++ src/SecretSanta.Models/Group.cs
@@ -4,7 +4,20 @@ namespace SecretSanta.Models
 {
     public class Group
     {
+        public Group()
+        {
+
+        }
+
+        public Group(string groupName, string ownerId)
+        {
+            this.GroupName = groupName;
+            this.OwnerId = ownerId;
+        }
+
         public int Id { get; set; }
+
+        public string GroupName { get; set; }
 
         public string OwnerId { get; set; }
 
