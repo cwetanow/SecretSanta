381da4742d268e34eb238e68b9fd4cab36ffd397
Ivan <madritistaaa@gmail.com>
2017-12-08 11:12:19 +0200

    Create group controller

---
+++ src/SecretSanta.Web/Controllers/GroupController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Services.Contracts;
+using SecretSanta.Web.Infrastructure;
+using System;
+
+namespace SecretSanta.Web.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly IGroupService groupService;
+        private readonly IDtoFactory factory;
+
+        public GroupController(IGroupService groupService, IDtoFactory factory)
+        {
+            if (groupService == null)
+            {
+                throw new ArgumentNullException(nameof(groupService));
+            }
+
+            if (factory == null)
+            {
+                throw new ArgumentNullException(nameof(factory));
+            }
+
+            this.groupService = groupService;
+            this.factory = factory;
+        }
+    }
+}
