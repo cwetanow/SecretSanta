b700a61ea5f8f3fceffc3b67f84bfb828adc11d5
Ivan <madritistaaa@gmail.com>
2017-12-08 11:16:18 +0200

    Test controller constructor

---
+++ src/SecretSanta.Web.Tests/Controllers/GroupControllerTests/ConstructorTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+using SecretSanta.Services.Contracts;
+using SecretSanta.Web.Controllers;
+using SecretSanta.Web.Infrastructure;
+using System;
+
+namespace SecretSanta.Web.Tests.Controllers.GroupControllerTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void TestConstructor_PassEverything_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedService = new Mock<IGroupService>();
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            // Act
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object);
+
+            // Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void TestConstructor_PassServiceNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new GroupController(null, mockedFactory.Object));
+        }
+
+        [Test]
+        public void TestConstructor_PassFactoryNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedService = new Mock<IGroupService>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() => new GroupController(mockedService.Object, null));
+        }
+    }
+}
