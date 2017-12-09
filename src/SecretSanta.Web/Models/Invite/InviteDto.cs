f034470a4e24f65729cd4ba9d7b08c1b5cce83d0
Ivan <madritistaaa@gmail.com>
2017-12-09 18:45:02 +0200

    Create dtos

---
+++ src/SecretSanta.Web/Models/Invite/InviteDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SecretSanta.Models.Enums;
+using SecretSanta.Web.Models.Users;
+
+namespace SecretSanta.Web.Models.Invite
+{
+    public class InviteDto
+    {
+        public InviteDto()
+        {
+
+        }
+
+        public InviteDto(InviteState state, DateTime date)
+        {
+            this.State = state;
+            this.Date = date;
+        }
+
+        public InviteState State { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public static Func<SecretSanta.Models.Invite, InviteDto> FromInvite
+        {
+            get { return (invite) => new InviteDto(invite.State, invite.Date); }
+        }
+    }
+}
---
+++ src/SecretSanta.Web/Models/Invite/InviteListDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SecretSanta.Web.Models.Invite
+{
+    public class InviteListDto
+    {
+        public InviteListDto()
+        {
+            this.Invites = new List<InviteDto>();
+        }
+
+        public InviteListDto(IEnumerable<SecretSanta.Models.Invite> invites)
+            : this()
+        {
+            this.Invites = invites
+                .Select(InviteDto.FromInvite)
+                .ToList();
+        }
+
+        public IEnumerable<InviteDto> Invites { get; set; }
+    }
+}
