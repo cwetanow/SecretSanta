38202002313ec0790bdc4f4882b4e51eb4465479
Ivan <madritistaaa@gmail.com>
2017-12-11 10:14:47 +0200

    Test controller

---
+++ src/SecretSanta.Web.Tests/Controllers/GroupControllerTests/GetGroupUsersTests.cs
@@ -0,0 +1,246 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SecretSanta.Authentication.Contracts;
+using SecretSanta.Services.Contracts;
+using SecretSanta.Web.Controllers;
+using SecretSanta.Web.Infrastructure;
+using System.Threading.Tasks;
+using SecretSanta.Common;
+using SecretSanta.Models;
+using System.Collections.Generic;
+using SecretSanta.Web.Models.Users;
+
+namespace SecretSanta.Web.Tests.Controllers.GroupControllerTests
+{
+    [TestFixture]
+    public class GetGroupUsersTests
+    {
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupNameEmpty_ShouldReturnBadRequest(string groupName, string userId)
+        {
+            // Arrange
+            var mockedService = new Mock<IGroupService>();
+            var mockedFactory = new Mock<IDtoFactory>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            var result = await controller.GetGroupUsers(null);
+
+            // Assert
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupNameEmpty_ShouldSetCorrectMessage(string groupName, string userId)
+        {
+            // Arrange
+            var mockedService = new Mock<IGroupService>();
+            var mockedFactory = new Mock<IDtoFactory>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            var result = await controller.GetGroupUsers(null) as BadRequestObjectResult;
+
+            // Assert
+            Assert.AreSame(Constants.GroupNameCannotBeNull, result.Value);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupNameNotEmpty_ShouldCallAuthenticationProviderGetCurrentUserAsync(string groupName, string userId)
+        {
+            // Arrange
+            var group = new Group { OwnerId = userId };
+
+            var mockedService = new Mock<IGroupService>();
+            mockedService.Setup(s => s.GetByName(It.IsAny<string>())).Returns(group);
+
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            var user = new User { Id = userId };
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.GetCurrentUserAsync()).ReturnsAsync(user);
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            await controller.GetGroupUsers(groupName);
+
+            // Assert
+            mockedAuthenticationProvider.Verify(p => p.GetCurrentUserAsync(), Times.Once);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupNameNotEmpty_ShouldCallGroupServiceGetByName(string groupName, string userId)
+        {
+            // Arrange
+            var group = new Group { OwnerId = userId };
+
+            var mockedService = new Mock<IGroupService>();
+            mockedService.Setup(s => s.GetByName(It.IsAny<string>())).Returns(group);
+
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            var user = new User { Id = userId };
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.GetCurrentUserAsync()).ReturnsAsync(user);
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            await controller.GetGroupUsers(groupName);
+
+            // Assert
+            mockedService.Verify(s => s.GetByName(groupName), Times.Once);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupOwnerDoesntMatchUser_ShouldReturnForbidden(string groupName, string userId)
+        {
+            // Arrange
+            var group = new Group { OwnerId = string.Empty };
+
+            var mockedService = new Mock<IGroupService>();
+            mockedService.Setup(s => s.GetByName(It.IsAny<string>())).Returns(group);
+
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            var user = new User { Id = userId };
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.GetCurrentUserAsync()).ReturnsAsync(user);
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            var result = await controller.GetGroupUsers(groupName);
+
+            // Assert
+            Assert.IsInstanceOf<ForbidResult>(result);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupOwnerMatchesUser_ShouldCallServiceGetGroupUsers(string groupName, string userId)
+        {
+            // Arrange
+            var group = new Group { OwnerId = userId };
+
+            var mockedService = new Mock<IGroupService>();
+            mockedService.Setup(s => s.GetByName(It.IsAny<string>())).Returns(group);
+
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            var user = new User { Id = userId };
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.GetCurrentUserAsync()).ReturnsAsync(user);
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            await controller.GetGroupUsers(groupName);
+
+            // Assert
+            mockedService.Verify(s => s.GetGroupUsers(groupName), Times.Once);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupOwnerMatchesUser_ShouldCallFactoryCreate(string groupName, string userId)
+        {
+            // Arrange
+            var group = new Group { OwnerId = userId };
+
+            var user = new User { Id = userId };
+            var users = new List<User> { user };
+
+            var mockedService = new Mock<IGroupService>();
+            mockedService.Setup(s => s.GetByName(It.IsAny<string>())).Returns(group);
+            mockedService.Setup(s => s.GetGroupUsers(It.IsAny<string>())).Returns(users);
+
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.GetCurrentUserAsync()).ReturnsAsync(user);
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            await controller.GetGroupUsers(groupName);
+
+            // Assert
+            mockedFactory.Verify(f => f.CreateUsersListDto(users), Times.Once);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupOwnerMatchesUser_ShouldReturnOk(string groupName, string userId)
+        {
+            // Arrange
+            var group = new Group { OwnerId = userId };
+
+            var user = new User { Id = userId };
+            var users = new List<User>();
+
+            var mockedService = new Mock<IGroupService>();
+            mockedService.Setup(s => s.GetByName(It.IsAny<string>())).Returns(group);
+            mockedService.Setup(s => s.GetGroupUsers(It.IsAny<string>())).Returns(users);
+
+            var mockedFactory = new Mock<IDtoFactory>();
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.GetCurrentUserAsync()).ReturnsAsync(user);
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            var result = await controller.GetGroupUsers(groupName);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [TestCase("group name", "d547a40d-c45f-4c43-99de-0bfe9199ff95")]
+        [TestCase("name", "99ae8dd3-1067-4141-9675-62e94bb6caaa")]
+        public async Task TestGetGroupUsers_GroupOwnerMatchesUser_ShouldSetCorrectBody(string groupName, string userId)
+        {
+            // Arrange
+            var group = new Group { OwnerId = userId };
+
+            var user = new User { Id = userId };
+            var users = new List<User>();
+
+            var mockedService = new Mock<IGroupService>();
+            mockedService.Setup(s => s.GetByName(It.IsAny<string>())).Returns(group);
+            mockedService.Setup(s => s.GetGroupUsers(It.IsAny<string>())).Returns(users);
+
+            var dto = new UsersListDto();
+
+            var mockedFactory = new Mock<IDtoFactory>();
+            mockedFactory.Setup(f => f.CreateUsersListDto(It.IsAny<IEnumerable<User>>())).Returns(dto);
+
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(p => p.GetCurrentUserAsync()).ReturnsAsync(user);
+
+            var controller = new GroupController(mockedService.Object, mockedFactory.Object, mockedAuthenticationProvider.Object);
+
+            // Act
+            var result = await controller.GetGroupUsers(groupName) as OkObjectResult;
+
+            // Assert
+            Assert.AreSame(dto, result.Value);
+        }
+    }
+}
