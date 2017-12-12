53e5773b195ecf8591abdcbee2bf2ef17e5f8252
Ivan <madritistaaa@gmail.com>
2017-12-12 10:36:37 +0200

    Implement service logic

--- src/SecretSanta.Services/Contracts/IInvitationService.cs
+++ src/SecretSanta.Services/Contracts/IInvitationService.cs
@@ -1,10 +1,13 @@
 ﻿using SecretSanta.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SecretSanta.Services.Contracts
 {
     public interface IInviteService
     {
         IEnumerable<Invite> GetPendingInvites(string userId, bool orderByAscending, int limit, int offset);
+
+        Task<bool> SendInvite(int groupId, string userId);
     }
 }
