2f2a674b4068fd6cd406b17ac50de6cbfaa4c9c7
Ivan <madritistaaa@gmail.com>
2017-12-12 10:04:29 +0200

    Create dto

---
+++ src/SecretSanta.Web/Models/Group/GroupListDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SecretSanta.Web.Models.Group
+{
+    public class GroupListDto
+    {
+        public GroupListDto()
+        {
+            this.Groups = new List<GroupDto>();
+        }
+
+        public GroupListDto(IEnumerable<SecretSanta.Models.Group> groups)
+        {
+            this.Groups = groups
+                .Select(GroupDto.FromGroup)
+                .ToList();
+        }
+
+        public IEnumerable<GroupDto> Groups { get; set; }
+    }
+}
