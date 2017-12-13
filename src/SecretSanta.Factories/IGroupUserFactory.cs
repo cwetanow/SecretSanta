0af0610978943fa4d2332269cdcd39869d9167ff
Ivan <madritistaaa@gmail.com>
2017-12-13 10:32:22 +0200

    Create factory for group user

---
+++ src/SecretSanta.Factories/IGroupUserFactory.cs
@@ -0,0 +1,9 @@
+﻿using SecretSanta.Models;
+
+namespace SecretSanta.Factories
+{
+    public interface IGroupUserFactory
+    {
+        GroupUser CreateGroupUser(int groupId, string userId);
+    }
+}
