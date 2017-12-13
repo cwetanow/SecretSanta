d43bbb9a04de4b796459708bb39a66d9a3e0ec9a
Ivan <madritistaaa@gmail.com>
2017-12-13 10:32:41 +0200

    Add interface for service

---
+++ src/SecretSanta.Services/Contracts/IMembershipService.cs
@@ -0,0 +1,10 @@
+﻿using SecretSanta.Models;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Services.Contracts
+{
+    public interface IMembershipService
+    {
+        Task<GroupUser> JoinGroup(int groupId, string userId);
+    }
+}
