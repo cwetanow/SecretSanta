a650975848435da11136c73c0d6a860cfbed1788
Ivan <madritistaaa@gmail.com>
2017-12-14 11:46:54 +0200

    Add gift distributor

---
+++ src/SecretSanta.Providers/Contracts/IGiftDistributionProvider.cs
@@ -0,0 +1,10 @@
+﻿using SecretSanta.Models;
+using System.Collections.Generic;
+
+namespace SecretSanta.Providers.Contracts
+{
+    public interface IGiftDistributionProvider
+    {
+        IEnumerable<Gift> DistributeGifts(IEnumerable<User> groupUsers);
+    }
+}
