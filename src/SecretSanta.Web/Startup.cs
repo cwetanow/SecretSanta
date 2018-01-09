e284c1055e450513c1c474256ee261ea5a6ca760
Ivan <madritistaaa@gmail.com>
2018-01-09 19:03:15 +0200

    Add error handling

--- src/SecretSanta.Web/Startup.cs
+++ src/SecretSanta.Web/Startup.cs
@@ -33,190 +33,191 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace SecretSanta.Web
 {
-    public class Startup
-    {
-        private readonly AsyncLocal<Scope> scopeProvider = new AsyncLocal<Scope>();
-        private IKernel kernel;
-        private IServiceProvider provider;
+	public class Startup
+	{
+		private readonly AsyncLocal<Scope> scopeProvider = new AsyncLocal<Scope>();
+		private IKernel kernel;
+		private IServiceProvider provider;
 
-        private object Resolve(Type type) => this.kernel.Get(type);
-        private object RequestScope(IContext context) => scopeProvider.Value;
+		private object Resolve(Type type) => this.kernel.Get(type);
+		private object RequestScope(IContext context) => scopeProvider.Value;
 
-        public Startup(IConfiguration configuration)
-        {
-            this.Configuration = configuration;
-        }
+		public Startup(IConfiguration configuration)
+		{
+			this.Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<SecretSantaContext>(options =>
-             options.UseSqlServer(Configuration.GetConnectionString("Default")));
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<SecretSantaContext>(options =>
+			 options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
-            services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<SecretSantaContext>()
-                .AddDefaultTokenProviders();
+			services.AddIdentity<User, IdentityRole>()
+				.AddEntityFrameworkStores<SecretSantaContext>()
+				.AddDefaultTokenProviders();
 
-            services.AddScoped<IDbContext>(serviceProvider => this.Get<SecretSantaContext>());
-            services.AddScoped<IAuthenticationProvider, AuthenticationProvider>();
-            services.AddSingleton<ITokenProvider, JwtTokenProvider>();
-            services.AddSingleton<ITokenManager, TokenManager>();
-            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+			services.AddScoped<IDbContext>(serviceProvider => this.Get<SecretSantaContext>());
+			services.AddScoped<IAuthenticationProvider, AuthenticationProvider>();
+			services.AddSingleton<ITokenProvider, JwtTokenProvider>();
+			services.AddSingleton<ITokenManager, TokenManager>();
+			services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
-            var policy = new AuthorizationPolicyBuilder()
-                .RequireAuthenticatedUser()
-                .Build();
+			var policy = new AuthorizationPolicyBuilder()
+				.RequireAuthenticatedUser()
+				.Build();
 
-            services
-                .AddAuthentication(opts =>
-                {
-                    opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer((config) =>
-                {
-                    config.RequireHttpsMetadata = false;
-                    config.SaveToken = true;
-                    config.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidIssuer = Configuration[Constants.TokenIssuer],
-                        ValidAudience = Configuration[Constants.TokenIssuer],
-                        IssuerSigningKey =
-                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration[Constants.TokenKey]))
-                    };
-                });
+			services
+				.AddAuthentication(opts =>
+				{
+					opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+					opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				})
+				.AddJwtBearer((config) =>
+				{
+					config.RequireHttpsMetadata = false;
+					config.SaveToken = true;
+					config.TokenValidationParameters = new TokenValidationParameters()
+					{
+						ValidIssuer = Configuration[Constants.TokenIssuer],
+						ValidAudience = Configuration[Constants.TokenIssuer],
+						IssuerSigningKey =
+							new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration[Constants.TokenKey]))
+					};
+				});
 
 
-            services.AddMvc(options =>
-            {
-                options.Filters.Add(new AuthorizeFilter(policy));
-            });
+			services.AddMvc(options =>
+			{
+				options.Filters.Add(new AuthorizeFilter(policy));
+			});
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            services.AddRequestScopingMiddleware(() => scopeProvider.Value = new Scope());
+			services.AddRequestScopingMiddleware(() => scopeProvider.Value = new Scope());
 
-            services.AddCustomControllerActivation(Resolve);
-            services.AddCustomViewComponentActivation(Resolve);
-        }
+			services.AddCustomControllerActivation(Resolve);
+			services.AddCustomViewComponentActivation(Resolve);
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider provider)
-        {
-            this.provider = provider;
-            this.kernel = this.RegisterApplicationComponents(app);
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider provider)
+		{
+			this.provider = provider;
+			this.kernel = this.RegisterApplicationComponents(app);
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseAuthentication();
+			app.UseAuthentication();
 
-            app.UseMvc();
-        }
+			app.UseMiddleware(typeof(ErrorHandlingMiddleware));
+			app.UseMvc();
+		}
 
-        private IKernel RegisterApplicationComponents(IApplicationBuilder app)
-        {
-            var kernel = new StandardKernel();
+		private IKernel RegisterApplicationComponents(IApplicationBuilder app)
+		{
+			var kernel = new StandardKernel();
 
-            kernel.Load(new FuncModule());
+			kernel.Load(new FuncModule());
 
-            foreach (var ctrlType in app.GetControllerTypes())
-            {
-                kernel.Bind(ctrlType)
-                    .ToSelf()
-                    .InScope(RequestScope);
-            }
+			foreach (var ctrlType in app.GetControllerTypes())
+			{
+				kernel.Bind(ctrlType)
+					.ToSelf()
+					.InScope(RequestScope);
+			}
 
-            // Factories
-            kernel.Bind<IUserFactory>()
-                .ToFactory()
-                .InSingletonScope();
+			// Factories
+			kernel.Bind<IUserFactory>()
+				.ToFactory()
+				.InSingletonScope();
 
-            kernel.Bind<IGroupFactory>()
-                .ToFactory()
-                .InSingletonScope();
+			kernel.Bind<IGroupFactory>()
+				.ToFactory()
+				.InSingletonScope();
 
-            kernel.Bind<IGroupUserFactory>()
-                .ToFactory()
-                .InSingletonScope();
+			kernel.Bind<IGroupUserFactory>()
+				.ToFactory()
+				.InSingletonScope();
 
-            kernel.Bind<IInviteFactory>()
-                .ToFactory()
-                .InSingletonScope();
+			kernel.Bind<IInviteFactory>()
+				.ToFactory()
+				.InSingletonScope();
 
-            kernel.Bind<IGiftFactory>()
-                .ToFactory()
-                .InSingletonScope();
+			kernel.Bind<IGiftFactory>()
+				.ToFactory()
+				.InSingletonScope();
 
-            kernel.Bind<IDtoFactory>()
-                .ToFactory()
-                .InSingletonScope();
+			kernel.Bind<IDtoFactory>()
+				.ToFactory()
+				.InSingletonScope();
 
-            // Authentication
-            kernel.Bind<IAuthenticationProvider>()
-                .ToMethod((context => this.Get<IAuthenticationProvider>()))
-                .InScope(RequestScope);
+			// Authentication
+			kernel.Bind<IAuthenticationProvider>()
+				.ToMethod((context => this.Get<IAuthenticationProvider>()))
+				.InScope(RequestScope);
 
-            // Data
-            kernel.Bind<IDbContext>()
-                .To<SecretSantaContext>()
-                .InScope(RequestScope)
-                .WithConstructorArgument(typeof(DbContextOptions), this.Get<DbContextOptions>());
+			// Data
+			kernel.Bind<IDbContext>()
+				.To<SecretSantaContext>()
+				.InScope(RequestScope)
+				.WithConstructorArgument(typeof(DbContextOptions), this.Get<DbContextOptions>());
 
-            kernel.Bind(typeof(IRepository<>))
-                .To(typeof(EfRepository<>))
-                .InScope(RequestScope);
+			kernel.Bind(typeof(IRepository<>))
+				.To(typeof(EfRepository<>))
+				.InScope(RequestScope);
 
-            kernel.Bind<IUnitOfWork>()
-                .To<UnitOfWork>()
-                .InScope(RequestScope);
+			kernel.Bind<IUnitOfWork>()
+				.To<UnitOfWork>()
+				.InScope(RequestScope);
 
-            // Services
-            kernel.Bind<IUserService>()
-                .To<UserService>()
-                .InScope(RequestScope);
+			// Services
+			kernel.Bind<IUserService>()
+				.To<UserService>()
+				.InScope(RequestScope);
 
-            kernel.Bind<IGiftService>()
-                .To<GiftService>()
-                .InScope(RequestScope);
+			kernel.Bind<IGiftService>()
+				.To<GiftService>()
+				.InScope(RequestScope);
 
-            kernel.Bind<IMembershipService>()
-                .To<MembershipService>()
-                .InScope(RequestScope);
+			kernel.Bind<IMembershipService>()
+				.To<MembershipService>()
+				.InScope(RequestScope);
 
-            kernel.Bind<IGroupService>()
-                .To<GroupService>()
-                .InScope(RequestScope);
+			kernel.Bind<IGroupService>()
+				.To<GroupService>()
+				.InScope(RequestScope);
 
-            kernel.Bind<IInviteService>()
-                .To<InviteService>()
-                .InScope(RequestScope);
+			kernel.Bind<IInviteService>()
+				.To<InviteService>()
+				.InScope(RequestScope);
 
-            // Providers
-            kernel.Bind<IDateTimeProvider>()
-                .To<DateTimeProvider>()
-                .InSingletonScope();
+			// Providers
+			kernel.Bind<IDateTimeProvider>()
+				.To<DateTimeProvider>()
+				.InSingletonScope();
 
-            kernel.Bind<IGiftManager>()
-                .To<GiftManager>()
-                .InSingletonScope();
+			kernel.Bind<IGiftManager>()
+				.To<GiftManager>()
+				.InSingletonScope();
 
-            // Cross-wire required framework services
-            kernel.BindToMethod(app.GetRequestService<IViewBufferScope>);
+			// Cross-wire required framework services
+			kernel.BindToMethod(app.GetRequestService<IViewBufferScope>);
 
-            return kernel;
-        }
+			return kernel;
+		}
 
-        private T Get<T>()
-        {
-            return (T)this.provider.GetService(typeof(T));
-        }
+		private T Get<T>()
+		{
+			return (T)this.provider.GetService(typeof(T));
+		}
 
-        private sealed class Scope : DisposableObject { }
-    }
+		private sealed class Scope : DisposableObject { }
+	}
 }
