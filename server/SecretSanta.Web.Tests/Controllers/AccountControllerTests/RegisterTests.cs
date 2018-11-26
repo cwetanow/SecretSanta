7d021d1b661520898b8de361ef1f419772fc7496
Ivan <madritistaaa@gmail.com>
2018-11-26 19:38:00 +0200

    Server: Fix tests

--- server/SecretSanta.Web.Tests/Controllers/AccountControllerTests/RegisterTests.cs
+++ server/SecretSanta.Web.Tests/Controllers/AccountControllerTests/RegisterTests.cs
@@ -10,265 +10,297 @@ using SecretSanta.Models;
 using SecretSanta.Web.Controllers;
 using SecretSanta.Web.Infrastructure;
 using SecretSanta.Web.Models.Account;
+using SecretSanta.Web.Models.Users;
 
 namespace SecretSanta.Web.Tests.Controllers.AccountControllerTests
 {
-    [TestFixture]
-    public class RegisterTests
-    {
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_ShouldCallAuthenticationProviderFindByUsernameAsyncCorrectly(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var mockedFactory = new Mock<IUserFactory>();
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            var mockedDtoFactory = new Mock<IDtoFactory>();
+	[TestFixture]
+	public class RegisterTests
+	{
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_ShouldCallAuthenticationProviderFindByUsernameAsyncCorrectly(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var mockedFactory = new Mock<IUserFactory>();
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            await controller.Register(model);
+			// Act
+			await controller.Register(model);
 
-            // Assert
-            mockedProvider.Verify(p => p.FindByUsernameAsync(username), Times.Once);
-        }
+			// Assert
+			mockedProvider.Verify(p => p.FindByUsernameAsync(username), Times.Once);
+		}
 
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_ThereIsUserWithUsername_ShouldReturnBadRequest(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var mockedFactory = new Mock<IUserFactory>();
-            var mockedDtoFactory = new Mock<IDtoFactory>();
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            mockedProvider.Setup(p => p.FindByUsernameAsync(It.IsAny<string>())).ReturnsAsync(new User());
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_ThereIsUserWithUsername_ShouldReturnBadRequest(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var mockedFactory = new Mock<IUserFactory>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			mockedProvider.Setup(p => p.FindByUsernameAsync(It.IsAny<string>())).ReturnsAsync(new User());
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            var result = await controller.Register(model);
+			// Act
+			var result = await controller.Register(model);
 
-            // Assert
-            Assert.IsInstanceOf<BadRequestObjectResult>(result);
-        }
+			// Assert
+			Assert.IsInstanceOf<BadRequestObjectResult>(result);
+		}
 
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_ThereIsUserWithUsername_ShouldReturnBadRequestWithCorrectMessage(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var mockedFactory = new Mock<IUserFactory>();
-            var mockedDtoFactory = new Mock<IDtoFactory>();
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            mockedProvider.Setup(p => p.FindByUsernameAsync(It.IsAny<string>())).ReturnsAsync(new User());
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_ThereIsUserWithUsername_ShouldReturnBadRequestWithCorrectMessage(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var mockedFactory = new Mock<IUserFactory>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			mockedProvider.Setup(p => p.FindByUsernameAsync(It.IsAny<string>())).ReturnsAsync(new User());
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            var result = await controller.Register(model) as BadRequestObjectResult;
+			// Act
+			var result = await controller.Register(model) as BadRequestObjectResult;
 
-            // Assert
-            Assert.AreSame(Constants.UserAlreadyExists, result.Value);
-        }
+			// Assert
+			Assert.AreSame(Constants.UserAlreadyExists, result.Value);
+		}
 
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_ThereIsNoUserWithUsername_ShouldCallFactoryCreate(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var mockedFactory = new Mock<IUserFactory>();
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            var mockedDtoFactory = new Mock<IDtoFactory>();
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_ThereIsNoUserWithUsername_ShouldCallFactoryCreate(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var mockedFactory = new Mock<IUserFactory>();
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            await controller.Register(model);
+			// Act
+			await controller.Register(model);
 
-            // Assert
-            mockedFactory.Verify(f => f.CreateUser(username, email, displayName), Times.Once);
-        }
+			// Assert
+			mockedFactory.Verify(f => f.CreateUser(username, email, displayName), Times.Once);
+		}
 
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_ThereIsNoUserWithUsername_ShouldCallAuthenticationProviderRegisterUserCorrectly(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var user = new User();
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_ThereIsNoUserWithUsername_ShouldCallAuthenticationProviderRegisterUserCorrectly(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var user = new User();
 
-            var mockedFactory = new Mock<IUserFactory>();
-            mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(user);
+			var mockedFactory = new Mock<IUserFactory>();
+			mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(user);
 
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            var mockedDtoFactory = new Mock<IDtoFactory>();
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            await controller.Register(model);
+			// Act
+			await controller.Register(model);
 
-            // Assert
-            mockedProvider.Verify(p => p.RegisterUser(user, password), Times.Once);
-        }
+			// Assert
+			mockedProvider.Verify(p => p.RegisterUser(user, password), Times.Once);
+		}
 
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_AuthenticationProviderRegisterIsNotSuccess_ShouldReturnBadRequest(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var user = new User();
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_AuthenticationProviderRegisterIsNotSuccess_ShouldReturnBadRequest(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var user = new User();
 
-            var mockedFactory = new Mock<IUserFactory>();
-            mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(user);
+			var mockedFactory = new Mock<IUserFactory>();
+			mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(user);
 
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            mockedProvider.Setup(p => p.RegisterUser(It.IsAny<User>(), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Failed());
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			mockedProvider.Setup(p => p.RegisterUser(It.IsAny<User>(), It.IsAny<string>()))
+				.ReturnsAsync(IdentityResult.Failed());
 
-            var mockedDtoFactory = new Mock<IDtoFactory>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            var result = await controller.Register(model);
+			// Act
+			var result = await controller.Register(model);
 
-            // Assert
-            Assert.IsInstanceOf<BadRequestResult>(result);
-        }
+			// Assert
+			Assert.IsInstanceOf<BadRequestResult>(result);
+		}
 
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_AuthenticationProviderRegisterIsSuccess_ShouldReturnOk(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var user = new User();
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_AuthenticationProviderRegisterIsSuccess_ShouldReturnOk(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var user = new User();
 
-            var mockedFactory = new Mock<IUserFactory>();
-            mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(user);
+			var mockedFactory = new Mock<IUserFactory>();
+			mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(user);
 
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            mockedProvider.Setup(p => p.RegisterUser(It.IsAny<User>(), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Success);
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			mockedProvider.Setup(p => p.RegisterUser(It.IsAny<User>(), It.IsAny<string>()))
+				.ReturnsAsync(IdentityResult.Success);
 
-            var mockedDtoFactory = new Mock<IDtoFactory>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            var result = await controller.Register(model);
+			// Act
+			var result = await controller.Register(model);
 
-            // Assert
-            Assert.IsInstanceOf<OkObjectResult>(result);
-        }
+			// Assert
+			Assert.IsInstanceOf<OkObjectResult>(result);
+		}
 
-        [TestCase("username", "email", "name", "password")]
-        public async Task TestRegister_AuthenticationProviderRegisterIsSuccess_ShouldReturnCorrectly(string username,
-            string email,
-            string displayName,
-            string password)
-        {
-            // Arrange
-            var user = new User();
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_AuthenticationProviderRegisterIsSuccess_ShouldCallDtoFactoryCreate(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var user = new User { UserName = username, Email = email, DisplayName = displayName };
 
-            var mockedFactory = new Mock<IUserFactory>();
-            mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(user);
+			var mockedFactory = new Mock<IUserFactory>();
+			mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(user);
 
-            var mockedProvider = new Mock<IAuthenticationProvider>();
-            mockedProvider.Setup(p => p.RegisterUser(It.IsAny<User>(), It.IsAny<string>()))
-                .ReturnsAsync(IdentityResult.Success);
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			mockedProvider.Setup(p => p.RegisterUser(It.IsAny<User>(), It.IsAny<string>()))
+				.ReturnsAsync(IdentityResult.Success);
 
-            var mockedDtoFactory = new Mock<IDtoFactory>();
+			var mockedDtoFactory = new Mock<IDtoFactory>();
 
-            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
 
-            var model = new RegisterDto
-            {
-                Username = username,
-                Email = email,
-                DisplayName = displayName,
-                Password = password
-            };
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
 
-            // Act
-            var result = await controller.Register(model);
+			// Act
+			await controller.Register(model);
 
-            // Assert
-            Assert.AreSame(user, (result as OkObjectResult).Value);
-        }
-    }
+			// Assert
+			mockedDtoFactory.Verify(f => f.CreateUserDto(user.UserName, user.Email, user.DisplayName), Times.Once);
+		}
+
+		[TestCase("username", "email", "name", "password")]
+		public async Task TestRegister_AuthenticationProviderRegisterIsSuccess_ShouldReturnCorrectly(string username,
+			string email,
+			string displayName,
+			string password)
+		{
+			// Arrange
+			var user = new User();
+
+			var mockedFactory = new Mock<IUserFactory>();
+			mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(user);
+
+			var mockedProvider = new Mock<IAuthenticationProvider>();
+			mockedProvider.Setup(p => p.RegisterUser(It.IsAny<User>(), It.IsAny<string>()))
+				.ReturnsAsync(IdentityResult.Success);
+
+			var dto = new UserDto();
+
+			var mockedDtoFactory = new Mock<IDtoFactory>();
+			mockedDtoFactory.Setup(f => f.CreateUserDto(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(dto);
+
+			var controller = new AccountController(mockedProvider.Object, mockedFactory.Object, mockedDtoFactory.Object);
+
+			var model = new RegisterDto {
+				Username = username,
+				Email = email,
+				DisplayName = displayName,
+				Password = password
+			};
+
+			// Act
+			var result = await controller.Register(model);
+
+			// Assert
+			Assert.AreSame(dto, (result as OkObjectResult).Value);
+		}
+	}
 }
