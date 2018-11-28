31b18ff7ca367bf95fbd223b53d9ad8388f9c37e
Ivan <madritistaaa@gmail.com>
2018-11-28 11:18:35 +0200

    Implement user service get list

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -6,6 +6,12 @@ class UserService {
   static getByUsername(username) {
     return requester.getAuthorized(`/users/${username}`)
   }
+
+  static getUsers(pattern = null, sortAscending = true, offset = 0, limit = 10) {
+    let url = `/users`;
+
+    return requester.getAuthorized(url);
+  }
 }
 
 export default UserService;
