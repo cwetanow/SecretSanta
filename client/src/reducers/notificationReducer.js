f4a6d6bf360a95eb4f47507c140baac92374fdd3
Ivan <madritistaaa@gmail.com>
2018-11-28 10:25:20 +0200

    Remove console.log

--- client/src/reducers/notificationReducer.js
+++ client/src/reducers/notificationReducer.js
@@ -1,7 +1,6 @@
 import * as types from '../actions/actionTypes';
 
 export default function notificationReducer(state = {}, action) {
-  console.log(action);
   switch (action.type) {
     case types.ERROR_NOTIFICATION:
       return Object.assign({}, action.notification)
