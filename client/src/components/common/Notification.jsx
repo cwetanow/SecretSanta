eb8ca0d5f215b4d26fbac2d8abedaf77adfeb82f
Ivan <madritistaaa@gmail.com>
2018-11-28 10:23:53 +0200

    Add map dispatch

--- client/src/components/common/Notification.jsx
+++ client/src/components/common/Notification.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import toastr from 'toastr';
 
 const Notification = (props) => {
-  if (props.notification.error) {
-    toastr.error(props.notification.error.message);
-  }
+  if (props && props.notification) {
+    if (props.notification.error) {
+      toastr.error(props.notification.error.message);
+    }
 
-  if (props.notification.success) {
-    toastr.success(props.notification.success.message);
+    if (props.notification.success) {
+      toastr.success(props.notification.success.message);
+    }
   }
 
   return (
@@ -15,4 +18,10 @@ const Notification = (props) => {
   );
 }
 
-export default Notification;
+const mapStateToProps = state => {
+  return {
+    notification: state.notification
+  };
+}
+
+export default connect(mapStateToProps)(Notification)
