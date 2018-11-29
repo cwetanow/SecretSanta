84bb5796cdc02d3eb3fb175a5f3f59df3c8b67ae
Ivan <madritistaaa@gmail.com>
2018-11-29 13:47:43 +0200

    Add modal to save group

--- client/src/components/group/CreateGroupModal.jsx
+++ client/src/components/group/CreateGroupModal.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Modal from 'react-bootstrap4-modal';
 import { Button, FormGroup, Input, Form, Col, Label, Row } from 'reactstrap';
-import { getUsers } from '../../actions/userActions';
 import { bindActionCreators } from 'redux';
 
 export class CreateGroupModal extends React.Component {
@@ -14,10 +13,14 @@ export class CreateGroupModal extends React.Component {
       }
     };
 
-    this.createGroup = this.createGroup.bind(this);
+    this.onGroupNameChange = this.onGroupNameChange.bind(this);
   }
 
-  createGroup() {
+  onGroupNameChange(newGroupName) {
+    const group = Object.assign({}, this.state.group);
+    group.groupName = newGroupName;
+
+    this.setState({ group });
   }
 
   render() {
@@ -27,14 +30,14 @@ export class CreateGroupModal extends React.Component {
           <Row>
             <Col md="6">
               <Label>Name</Label>
-              <Input onChange={this.onInputChange}
+              <Input onChange={(ev) => this.onGroupNameChange(ev.target.value)}
                 value={this.state.group.groupName} type="text" />
             </Col>
           </Row>
         </FormGroup>
 
         <FormGroup>
-          <Button type="submit" size="xl" color="primary" onClick={this.save}><i className="fa fa-dot-circle-o"></i> Save</Button>
+          <Button type="submit" size="xl" color="primary" onClick={() => this.props.createGroup(this.state.group)} disabled={!this.state.group.groupName}><i className="fa fa-dot-circle-o"></i> Save</Button>
           <Button size="xl" color="primary" className="float-right" onClick={this.props.closeModal}><i className="fa fa-dot-circle-o"></i> Close</Button>
         </FormGroup>
       </Form>
@@ -42,8 +45,4 @@ export class CreateGroupModal extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ getUsers }, dispatch)
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CreateGroupModal);
+export default CreateGroupModal;
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import GroupsList from '../group/GroupsList';
+import CreateGroupModal from '../group/CreateGroupModal';
 
-import { getJoinedGroups } from '../../actions/groupActions.js';
+import { getJoinedGroups, createGroup } from '../../actions/groupActions.js';
+
+import { Button } from 'reactstrap';
 
 class Home extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      isCreateGroupModalOpen: false
+    }
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.createGroup = this.createGroup.bind(this);
+  }
+
   componentDidMount() {
     this.props.getJoinedGroups();
   }
 
+  createGroup(group) {
+    this.props.createGroup(group)
+      .then(() => {
+        console.log(group)
+      })
+  }
+
+  toggleModal() {
+    this.setState({ isCreateGroupModalOpen: !this.state.isCreateGroupModalOpen });
+  }
+
   render() {
     return (<div>
+      <Button type="submit" size="xl" color="primary" onClick={this.toggleModal} > Create group</Button>
+      {this.state.isCreateGroupModalOpen && <CreateGroupModal closeModal={this.toggleModal} createGroup={this.createGroup} />}
       <GroupsList groups={this.props.joinedGroups} />
     </div>);
   }
@@ -24,7 +51,7 @@ const mapStateToProps = (state, ownProps) => {
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ getJoinedGroups }, dispatch)
+  return bindActionCreators({ getJoinedGroups, createGroup }, dispatch)
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
