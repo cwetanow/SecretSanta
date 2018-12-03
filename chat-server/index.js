546f83a0cfa380d1328d1c71a5a91c43c78f3872
Ivan <madritistaaa@gmail.com>
2018-12-03 17:12:25 +0200

    Add initial scaffold of node express backend

---
+++ chat-server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const config = require('./config');
+
+const app = express();
+app.server = http.createServer(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./config/db')(config);
+
+app.server.listen(config.port, () => {
+  console.log(`Started on port ${config.port}`);
+});
