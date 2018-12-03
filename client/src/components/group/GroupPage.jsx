789ee07162451f0cf00b4ec23b736207b630f087
Ivan <madritistaaa@gmail.com>
2018-12-03 11:14:32 +0200

    Add remove button for group users

--- client/src/components/group/GroupPage.jsx
+++ client/src/components/group/GroupPage.jsx
@@ -4,9 +4,10 @@ import { bindActionCreators } from 'redux';
 import GroupsList from '../group/GroupsList';
 import CreateGroupModal from '../group/CreateGroupModal';
 import { Redirect } from 'react-router-dom';
-import { getGroupUsers, checkGroupOwner } from '../../actions/groupActions.js';
+import { getGroupUsers, checkGroupOwner, removeUser } from '../../actions/groupActions.js';
 import { distributeGifts, getGift } from '../../actions/giftActions';
-import { Button, Container } from 'reactstrap';
+import { Button, Container, Row, Col, Card, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 import Gift from '../gift/Gift'
 import InviteUsers from '../invite/InviteUsers';
@@ -16,6 +17,7 @@ class GroupPage extends Component {
     super(props, context);
 
     this.distributeGifts = this.distributeGifts.bind(this);
+    this.renderGroupUsers = this.renderGroupUsers.bind(this);
   }
 
   componentDidMount() {
@@ -28,6 +30,27 @@ class GroupPage extends Component {
     this.props.distributeGifts(this.props.groupName);
   }
 
+  renderGroupUsers() {
+    return (<div>
+      Users
+      {this.props.groupUsers.map(user =>
+        <Row key={user.username} className="justify-content-center">
+          <Col md="6">
+            <Card className="mx-4">
+              <CardBody className="p-4">
+                <CardTitle>
+                  <Link to={'/users/' + user.username}>{user.displayName} ({user.username})</Link>
+                </CardTitle>
+                <CardSubtitle>
+                  {this.props.isUserOwner && <Button type="submit" size="xs" color="danger" onClick={() => this.props.removeUser(this.props.groupName, user)} >Remove user</Button>}
+                </CardSubtitle>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>)}
+    </div>)
+  }
+
   render() {
     return (
       <Container>
@@ -35,6 +58,8 @@ class GroupPage extends Component {
         <hr />
         {!this.props.hasGift && this.props.isUserOwner && <Button type="submit" size="xl" color="primary" onClick={this.distributeGifts} >Distribute gifts</Button>}
         {this.props.hasGift && <Gift gift={this.props.gift} />}
+
+        {!this.props.hasGift && this.renderGroupUsers()}
       </Container>
     );
   }
@@ -45,12 +70,13 @@ const mapStateToProps = (state, ownProps) => {
     groupName: ownProps.match.params.groupName,
     isUserOwner: state.group.isUserOwner,
     hasGift: state.gift.groupGift && state.gift.groupGift.hasGift,
-    gift: state.gift.groupGift && state.gift.groupGift.gift
+    gift: state.gift.groupGift && state.gift.groupGift.gift,
+    groupUsers: state.group.users || []
   };
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ getGroupUsers, checkGroupOwner, distributeGifts, getGift }, dispatch)
+  return bindActionCreators({ getGroupUsers, checkGroupOwner, distributeGifts, getGift, removeUser }, dispatch)
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(GroupPage);
