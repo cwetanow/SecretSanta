e08409842d367dcd5198024c6788e7888037b2f5
Ivan <madritistaaa@gmail.com>
2018-12-16 11:26:22 +0200

    Server: Add IsClosed flag to group

--- server/SecretSanta.Models/Group.cs
+++ server/SecretSanta.Models/Group.cs
@@ -2,29 +2,32 @@
 
 namespace SecretSanta.Models
 {
-    public class Group
-    {
-        public Group()
-        {
+	public class Group
+	{
+		public Group()
+		{
+			this.IsClosed = false;
+		}
 
-        }
+		public Group(string groupName, string ownerId)
+		: this()
+		{
+			this.GroupName = groupName;
+			this.OwnerId = ownerId;
+		}
 
-        public Group(string groupName, string ownerId)
-        {
-            this.GroupName = groupName;
-            this.OwnerId = ownerId;
-        }
+		public int Id { get; set; }
 
-        public int Id { get; set; }
+		public string GroupName { get; set; }
 
-        public string GroupName { get; set; }
+		public string OwnerId { get; set; }
 
-        public string OwnerId { get; set; }
+		public User Owner { get; set; }
 
-        public User Owner { get; set; }
+		public ICollection<GroupUser> Users { get; set; }
 
-        public ICollection<GroupUser> Users { get; set; }
+		public ICollection<Invite> Invites { get; set; }
 
-        public ICollection<Invite> Invites { get; set; }
-    }
+		public bool IsClosed { get; set; }
+	}
 }
