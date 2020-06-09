03d1eedf75667da4cfcb263bcd793b5bfd1862e3
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-06-09 10:35:05 +0300

    Add db context

---
+++ server/SecretSanta.Persistence/Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SecretSanta.Persistence.Extensions
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddPersistence(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction)
+		{
+			services
+				.AddDbContext<SecretSantaContext>(optionsAction)
+				.AddScoped<DbContext>(provider => provider.GetService<SecretSantaContext>());
+
+			return services;
+		}
+	}
+}
---
+++ server/SecretSanta.Persistence/SecretSantaContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SecretSanta.Domain.Common;
+
+namespace SecretSanta.Persistence
+{
+	public class SecretSantaContext : DbContext
+	{
+		public SecretSantaContext(DbContextOptions<SecretSantaContext> options)
+			: base(options)
+		{ }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+			{
+				// Configure primary key
+				if (typeof(Entity).IsAssignableFrom(entityType.ClrType))
+				{
+					modelBuilder.Entity(entityType.ClrType)
+						.HasKey(nameof(Entity.Id));
+				}
+			}
+
+			// Disable cascade delete
+			modelBuilder.Model.GetEntityTypes()
+				.SelectMany(t => t.GetForeignKeys())
+				.Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade)
+				.ToList()
+				.ForEach(e => e.DeleteBehavior = DeleteBehavior.Restrict);
+
+			modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
