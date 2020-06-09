0feaaf15d0e18c7a7fc9a5bba9690b048061fcf3
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-06-09 11:06:42 +0300

    Add interface for db context

---
+++ server/SecretSanta.Application/Common/Interfaces/ISecretSantaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using SecretSanta.Domain.Common;
+
+namespace SecretSanta.Application.Common.Interfaces
+{
+	public interface ISecretSantaDbContext
+	{
+		public IQueryable<TEntity> Set<TEntity>();
+
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+		public void Add<TEntity>(TEntity entity)
+			where TEntity : Entity;
+	}
+}
