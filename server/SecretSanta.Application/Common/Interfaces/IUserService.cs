2859138aee81bebf100c12f071900866b4036475
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-06-09 11:06:32 +0300

    Add interface for user service

---
+++ server/SecretSanta.Application/Common/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SecretSanta.Application.Common.Models;
+
+namespace SecretSanta.Application.Common.Interfaces
+{
+	public interface IUserService
+	{
+		Task<(Result result, string userId)> CreateUser(string username, string email, string password);
+	}
+}
