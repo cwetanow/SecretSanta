a128e0f20e7f3c2af0b88f4734bde34c897989b8
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-06-09 11:06:55 +0300

    Add user entity

---
+++ server/SecretSanta.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using SecretSanta.Domain.Common;
+
+namespace SecretSanta.Domain.Entities
+{
+	public class User : Entity
+	{
+		private User() { }
+
+		public User(string userId, string username, string email, string displayName)
+		{
+			UserId = userId;
+			Username = username;
+			Email = email;
+			DisplayName = displayName;
+		}
+
+		public string UserId { get; }
+		public string Username { get; }
+		public string Email { get; }
+		public string DisplayName { get; }
+	}
+}
