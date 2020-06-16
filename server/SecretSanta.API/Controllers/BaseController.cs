84428939f0ec3ea3d079c84e4d28393562f409ff
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-06-16 10:19:17 +0300

    Add base controller

---
+++ server/SecretSanta.API/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretSanta.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public abstract class BaseController : ControllerBase
+	{
+		protected readonly IMediator Mediator;
+
+		protected BaseController(IMediator mediator)
+		{
+			this.Mediator = mediator;
+		}
+	}
+}
