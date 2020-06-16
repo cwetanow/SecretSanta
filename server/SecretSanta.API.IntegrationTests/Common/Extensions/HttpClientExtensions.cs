456cb981e3c03d60580b92751b5399c7551a86c1
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-06-16 15:34:48 +0300

    Add infrastructure for integartion tests

---
+++ server/SecretSanta.API.IntegrationTests/Common/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SecretSanta.API.IntegrationTests.Common.Extensions
+{
+	public static class HttpClientExtensions
+	{
+		public static async Task<TResponse> PostJson<TResponse>(this HttpClient client, string url, object body)
+		{
+			var content = new StringContent(JsonConvert.SerializeObject(body));
+
+			content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+			// Act
+			var response = await client.PostAsync(url, content);
+
+			var responseContent = await response.Content.ReadAsStringAsync();
+			return JsonConvert.DeserializeObject<TResponse>(responseContent);
+		}
+	}
+}
