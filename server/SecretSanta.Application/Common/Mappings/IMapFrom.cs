41db97cc2c29e4f3d3fa4c78a7631bd2e887fdda
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-07-16 13:54:13 +0300

    Add automapper configurations

---
+++ server/SecretSanta.Application/Common/Mappings/IMapFrom.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace SecretSanta.Application.Common.Mappings
+{
+	public interface IMapFrom<T>
+	{
+		void ApplyMapping(Profile profile) => profile.CreateMap(typeof(T), this.GetType());
+	}
+}
