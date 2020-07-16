debd44c162b06ad5c5388daeb715953713c6725a
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-07-16 13:54:29 +0300

    Add command for authenticate user

---
+++ server/SecretSanta.Application/Users/Responses/UserProfileResponse.cs
@@ -0,0 +1,13 @@
+﻿using SecretSanta.Application.Common.Mappings;
+using SecretSanta.Domain.Entities;
+
+namespace SecretSanta.Application.Users.Responses
+{
+	public class UserProfileResponse : IMapFrom<User>
+	{
+		public int Id { get; set; }
+		public string DisplayName { get; set; }
+		public string Email { get; set; }
+		public string Username { get; set; }
+	}
+}
