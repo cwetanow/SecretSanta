0ddff6f84c35065bcee9cb136b18199bcac96dce
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-07-16 13:54:56 +0300

    Implement controller action

--- server/SecretSanta.API/Controllers/IdentityController.cs
+++ server/SecretSanta.API/Controllers/IdentityController.cs
@@ -2,18 +2,37 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SecretSanta.API.Models.Responses;
 using SecretSanta.Application.Users.Commands;
+using SecretSanta.Identity.Interfaces;
 
 namespace SecretSanta.API.Controllers
 {
 	public class IdentityController : BaseController
 	{
-		public IdentityController(IMediator mediator) : base(mediator)
-		{ }
+		private readonly ITokenService tokenService;
+
+		public IdentityController(IMediator mediator, ITokenService tokenService)
+			: base(mediator)
+		{
+			this.tokenService = tokenService;
+		}
 
 		[HttpPost]
 		[Route("api/users")]
 		[AllowAnonymous]
 		public async Task<int> RegisterUser([FromBody] RegisterUserCommand request) => await Mediator.Send(request);
+
+		[HttpPost]
+		[AllowAnonymous]
+		[Route("api/login")]
+		public async Task<LoginResponse> LoginUser([FromBody]AuthenticateUserCommand request)
+		{
+			var user = await Mediator.Send(request);
+			
+			var token = tokenService.EncodeToken(user.Username, user.Id, user.DisplayName);
+
+			return new LoginResponse(token);
+		}
 	}
 }
