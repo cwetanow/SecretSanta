16b2a446a0da08ef447e009e6a37ca54457389ff
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-07-29 16:40:34 +0300

    Create response object

---
+++ server/SecretSanta.Application/Users/Responses/UserListResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SecretSanta.Application.Common.Mappings;
+
+namespace SecretSanta.Application.Users.Responses
+{
+	public class UserListResponse : IMapFrom<IEnumerable<UserProfileResponse>>
+	{
+		public IEnumerable<UserProfileResponse> Users { get; set; }
+	}
+}
