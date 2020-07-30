7e1f36befbdb15e83e8509b36b27738ad8be40b3
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-07-30 11:15:51 +0300

    Add query with validator

---
+++ server/SecretSanta.Application/Users/Queries/UserByUsernameQuery.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MediatR;
+using SecretSanta.Application.Users.Responses;
+
+namespace SecretSanta.Application.Users.Queries
+{
+	public class UserByUsernameQuery : IRequest<UserProfileResponse>
+	{
+		public string Username { get; set; }
+
+		public class Validator : AbstractValidator<UserByUsernameQuery>
+		{
+			public Validator()
+			{
+				RuleFor(q => q.Username).NotEmpty();
+			}
+		}
+	}
+}
